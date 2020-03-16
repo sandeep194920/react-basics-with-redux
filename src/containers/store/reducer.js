74c0f36b574635eb0b099cf35ebc2cbc822f5583
sandeep194920 <sandeepamaranath@gmail.com>
2020-03-16 13:23:41 -0400

    dispatched first action - to increment

--- src/containers/store/reducer.js
+++ src/containers/store/reducer.js
@@ -1,8 +1,15 @@
 const initialState = {
-  counter: 12
+  counter: 0
 };
 
 const reducer = (state = initialState, action) => {
+  // the dispatched actions in the components can be handled here using action argument.
+  if (action.type === "INCREMENT") {
+    return {
+      // this return object must be a state (modified or new one)
+      counter: state.counter + 1
+    };
+  }
   return state;
 };
 
