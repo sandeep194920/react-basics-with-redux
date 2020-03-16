525b2239905fb44405fdc28f0c7a4d02a16bafd2
sandeep194920 <sandeepamaranath@gmail.com>
2020-03-16 15:16:23 -0400

    replaced if statements with switch statements

--- src/containers/store/reducer.js
+++ src/containers/store/reducer.js
@@ -3,32 +3,28 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-  // the dispatched actions in the components can be handled here using action argument.
-  if (action.type === "INCREMENT") {
-    return {
-      // this return object must be a state (modified or new one)
-      counter: state.counter + 1
-    };
+  // Note that I havent added break statement to any case since it would be unreachable as I am returning the value in
+  // each case.
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        counter: state.counter + 1
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1
+      };
+    case "ADD":
+      return {
+        counter: state.counter + action.value
+      };
+    case "SUBTRACT":
+      return {
+        counter: state.counter - action.value
+      };
+    default:
+      return state;
   }
-  if (action.type === "DECREMENT") {
-    return {
-      // this return object must be a state (modified or new one)
-      counter: state.counter - 1
-    };
-  }
-  if (action.type === "ADD") {
-    return {
-      // this return object must be a state (modified or new one)
-      counter: state.counter + action.value
-    };
-  }
-  if (action.type === "SUBTRACT") {
-    return {
-      // this return object must be a state (modified or new one)
-      counter: state.counter - action.value
-    };
-  }
-  return state;
 };
 
 export default reducer;
